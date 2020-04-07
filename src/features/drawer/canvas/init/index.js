3718f1d62fe4fb02109f105cf3cd26f2ba6065a0
Stuart Leitch <Toruitas@gmail.com>
2020-04-07 12:31:45 +0100

    updatePosition updated for pixel canvas

--- src/features/drawer/canvas/init/index.js
+++ src/features/drawer/canvas/init/index.js
@@ -36,13 +36,13 @@ export default (id) => {
 
     const modelRender = new ModelRenderer();
     modelRender.registerNewModel(new ModelType(vertices, indices), "triangle");
-    const instance = new ModelInstance(0, 0, 0, 0, 0, 0, 1.0);
+    const instance = new ModelInstance(0, 0, 0, 0, 0, 0, 1.0, gl.canvas.width, gl.canvas.height);
     modelRender.addInstance(instance, 'triangle');
 
     const render = () => {
         GLM.clear(1.0, 1.0, 1.0, 1.0);
-        instance.updateRotation(180, 0, 0);  
-        // instance.updatePosition(0.5, 0.05, 0);
+        instance.updateRotation(0, 180, 0);  
+        instance.updatePosition(500, 300, 0);
         modelRender.render();
         window.requestAnimationFrame(render);
     }
--- src/features/drawer/canvas/models/modelInstance/index.js
+++ src/features/drawer/canvas/models/modelInstance/index.js
@@ -4,19 +4,25 @@ export default class ModelInstance {
     // It's important to keep in mind that the transformation matrix will be drawn in (-1,1)
     // So these must be converted to that scale 
 
-    constructor(x, y, z, rx, ry, rz, scale){
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    constructor(x, y, z, rx, ry, rz, scale, width, height){
+        this.x = this.pixelToClip(x,width);
+        this.y = this.pixelToClip(y,height);
+        this.z = z; // Z we leave as -1,1 for now.
         this.rx = this.degreeToClip(rx);
         this.ry = this.degreeToClip(ry);
         this.rz = this.degreeToClip(rz);
+        this.width = width;
+        this.height = height;
         this.scale = scale;
     }
 
     degreeToClip = (degree) => {
         // a clipspace rotation of 1 = 1 radian = 180*        
         return degree/180;
+    }
+
+    pixelToClip = (pixelPos, dimension) =>{
+        return pixelPos/dimension;
     }
 
     updateRotation = (rx, ry, rz) => {
@@ -28,8 +34,8 @@ export default class ModelInstance {
     }
 
     updatePosition = (x, y, z) => {
-        this.x = x;
-        this.y = y;
+        this.x = this.pixelToClip(x, this.width);
+        this.y = this.pixelToClip(y,this.height);
         this.z = z;
         this.updateTransformationMatrix();
     }
